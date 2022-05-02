8dd6312d7df016873f22b7c625180acfc2230f1c
najmulhouda <najmulhouda6@gmail.com>
2022-05-02 13:11:27 +0600

    second commit from najmul

--- src/components/PricingPage.js
+++ src/components/PricingPage.js
@@ -1,12 +1,27 @@
 import MainMenu from "./common/MainMenu";
+import './PricingPage.css'
 
 function PricingPage() {
   return (
-    <div className="landing">
+    <div className="landing background">
       <header className="App-header">
         <MainMenu />
       </header>
-      Pricing Page .....
+      <div className="container">
+          <div className="text-center text-white">
+            <p className="h1">Simple & Transparent Pricing</p>
+            <p>No surprise fees.</p>
+            <p className="d-flex justify-content-center"><span>Billed yearly</span>
+            <div className="form-check form-switch mx-2">
+              <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+              <label className="form-check-label" for="flexSwitchCheckChecked"></label>
+            </div>
+            <span className="checked-color">Billed Monthly</span>
+            </p>
+
+          </div>
+         
+      </div>
     </div>
   );
 }
