9528c7649eafefbe78c42962d1a1c4eab69d8746
Nushrat-Nokshi <nushratahmed1814@gmail.com>
2022-05-07 22:47:50 +0600

    additing google font and others updates

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,6 @@
 import MainMenu from "./common/MainMenu";
+import "./LandingPage.css";
+import Logo from '..//images/Lucy\ Logo.png';
 
 function LandingPage() {
   return (
@@ -6,8 +8,26 @@ function LandingPage() {
       <header className="App-header">
         <MainMenu />
       </header>
-      Landing Page...
+      <div id="back-drop" className="card mb-4 mt-5 h-15">
+          <div className="Logo">
+            <img src={Logo}  alt="Logo"/>
+          </div>
+        <div className="container m-5" >
+          <div className="card-body ">
+            <h3 className="card-title"  >Thank you for helping <br/>   us with this alpha test.</h3><br/>
+            <p className="card-text">It will take only a few minutes to try out.<br/>
+            We think you've never seen anything like Lucy™, <br/>
+            and we're sure you'll at least have some fun with it.</p>
+            <br/>
+            <div className="d-flex ">
+              <button id="btn1" type="button" className="btn btn-sm">Get Started Here</button>
+              <button id="btn2" type="button" className="btn btn-sm">What is Lucy™?</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+     
   );
 }
 
